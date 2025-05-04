9c665e7207996cbf66022f3eb407e2f9c72e53fd
faleroid <naufalsatrioputra@gmail.com>
2025-05-04 11:07:59 +0700

    second commit

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<?php
+
+$name = "pangpiwwww";
+$age = 19;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bismillah</title>
+</head>
+<body>
+    <header>
+        <h1>Halooooo <?php echo $name?> </h1>
+        <h3>Umurnya <?php echo $age?> </h3>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
